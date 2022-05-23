06c76229144b882c5fde7277e494fecf4e555c98
jun-yeong <32181854@dankook.ac.kr>
2022-05-23 21:09:23 +0900

    2022.05.23.model_완성

--- model.py
+++ model.py
@@ -1,23 +1,24 @@
 import torch
 import torch.nn as nn
 
+
 # 1-dimensional CNN
 class CNN(nn.Module):
-    def __init__(self, max_seq_len, dropout_rate=0, vocab_size=1751, embedding_dim=100):
+    def __init__(self, max_seq_len, vocab_size=1751, embedding_dim=100):
         super(CNN, self).__init__()
         self.max_seq_len = max_seq_len              # sentence length
         self.embedding_dim = embedding_dim          # GloVe, Word2Vec 기준으로 범위 잡기
-        self.dropout_rate = dropout_rate
 
         # self.embbed = nn.Embedding(vocab_size, embedding_dim)
         # self.multi_size_conv = [nn.Conv1d(in_channels=1, out_channels=1, kernel_size=(kernel_size, embedding_dim), stride=1) for kernel_size in range(1, 6)]     # first conv. layer
         # self.uni_size_conv = nn.Conv1d(in_channels=1, out_channels=1, kernel_size=(3, embedding_dim), stride=1)
         # self.cnn = nn.ModuleList([self.multi_size_conv, self.uni_size_conv, self.uni_size_conv])
 
+        self.conv1 = nn.ModuleList([nn.Conv1d(in_channels=self.embedding_dim, out_channels=128, kernel_size=kernel) for kernel in range(1, 6)])       # First CNN layers
         self.relu = nn.ReLU()
+        self.conv2 = nn.Conv1d(in_channels=128, out_channels=128, kernel_size=3)                  # 논문 '그림 상'으로는 128이 in channel이므로 reshape 안함
+        self.conv3 = nn.Conv1d(in_channels=128, out_channels=128, kernel_size=3)
 
-        self.conv1 = nn.ModuleList([nn.Conv1d(in_channels=self.embedding_dim, out_channels=128,
-                                                    kernel_size=kernel) for kernel in range(1, 6)])       # First CNN layers
         self.max1d = None
 
     def max_pooling(self, input, kernel, stride):
@@ -26,14 +27,13 @@ class CNN(nn.Module):
 
         return ret
 
-    def conv(self, input, in_channel, kernel, out_channel=128):
-        layer = nn.Conv1d(in_channels=in_channel, out_channels=out_channel, kernel_size=kernel)
-        ret = layer(input)
+    # def conv(self, input, in_channel, kernel, out_channel=128):
+    #     layer = nn.Conv1d(in_channels=in_channel, out_channels=out_channel, kernel_size=kernel)
+    #     ret = layer(input)
+    #
+    #     return ret
 
-        return ret
-
-    def forward(self, input):           # data loader 어떻게 사용하는지 추가하기
-
+    def forward(self, input):
         input = input.permute(0, 2, 1)
 
         x = []
@@ -44,48 +44,61 @@ class CNN(nn.Module):
             x.append(feature)
 
         x = torch.cat(x, dim=2)
-        x = self.conv(x, in_channel=x.size(dim=1), kernel=3)      # 논문 '그림 상'으로는 128이 in channel이므로 reshape 안함
+
+        x = self.conv2(x)
         x = self.relu(x)
         x = self.max_pooling(x, kernel=2, stride=2)
 
-        x = self.conv(x, in_channel=x.size(dim=1), kernel=3)
+        x = self.conv3(x)
         x = self.relu(x)
         x = self.max_pooling(x, kernel=2, stride=2)
 
         return x
 
-    def call(self):
-        ret = self.forward()
-
-        return ret
-
 
 class BiRNN(nn.Module):
-    def __init__(self, model, max_seq_len, embedding_dim=100):
+    def __init__(self, model, max_seq_len, dropout_rate, merge_mode=None, embedding_dim=100):
         super(BiRNN, self).__init__()
         self.model = model
         self.embedding_dim = embedding_dim
-        self.hidden_size = 100                # 논문에 언급 없음
-        self.hidden_units = 128
+        self.hidden_size = 128                # 논문에 언급 없음
+        self.max_seq_len = max_seq_len
+        self.dropout_rate = dropout_rate
+        self.merge_mode = merge_mode
 
         self.bi_gru = nn.GRU(input_size=self.embedding_dim, hidden_size=self.hidden_size,
-                             num_layers=self.hidden_units, batch_first=True, bidirectional=True)
+                             num_layers=self.max_seq_len, batch_first=True, dropout=self.dropout_rate, bidirectional=True)
 
         self.bi_lstm = nn.LSTM(input_size=self.embedding_dim, hidden_size=self.hidden_size,
-                               num_layers=self.hidden_units, batch_first=True, bidirectional=True)
+                               num_layers=self.max_seq_len, batch_first=True, bidirectional=True)
+
+        self.hidden = None
 
     def forward(self, x):
         if self.model == "gru":
-            output, hidden = self.bi_gru(x)
+            output, self.hidden = self.bi_gru(x)
         elif self.model == "lstm":
-            output, hidden = self.bi_lstm(x)
+            output, self.hidden = self.bi_lstm(x)
 
-        return output, hidden
+        # # 파라미터 개수 확인 코드
+        # cnt = 0
+        # for param in model.parameters():
+        #     param = torch.flatten(param)
+        #     cnt += param.size(dim=0)
+        # print("bigru weight counting: ", cnt)
+        # bigru weight counting:  20763266, summary에 안뜨는건 GRU인지 LSTM인지 결정이 안되서 그런듯
 
-    def call(self):
-        ret = self.forward()
 
-        return ret
+        batch_size = x.size(dim=0)
+        hidden_forward = self.hidden.view(2, self.max_seq_len, batch_size, self.hidden_size)[0]
+        hidden_backward = self.hidden.view(2, self.max_seq_len, batch_size, self.hidden_size)[1]
+
+        if self.merge_mode == "add":
+            self.hidden = torch.add(hidden_forward, hidden_backward)
+        elif self.merge_mode == "mean":
+            self.hidden = torch.mean((hidden_forward, hidden_backward), dim=0)              # dimension 맞는지 재확인 필요
+
+        return self.hidden
 
 
 class Attention(nn.Module):         # feed-forward attention
@@ -94,13 +107,15 @@ class Attention(nn.Module):         # feed-forward attention
         self.d = d             # CNN: the number of filters = 128  /  Bi LSTM: dim of the contextual word embedding = 128
         # self.T = t             # length of generated feature map   /  Bi LSTM: the number of hidden states
 
+        self.feedforward = nn.Linear(in_features=self.d, out_features=1)
         self.tanh = nn.Tanh()
         self.softmax = nn.Softmax()
 
-        self.feedforward = nn.Linear(in_features=self.d, out_features=1)
-
     def forward(self, x):
-        x = torch.transpose(x, -1, -2)
+        if x.size(dim=-1) == 128:                   # BiGRU
+            x = x.permute(1, 0, 2)
+        else:                                       # CNN
+            x = torch.transpose(x, -1, -2)
 
         m = self.feedforward(x)
         m = self.tanh(m)
@@ -109,26 +124,24 @@ class Attention(nn.Module):         # feed-forward attention
 
         return r
 
-    def call(self):
-        ret = self.fowrad()
-
-        return ret
 
 class Model(nn.Module):
-    def __init__(self, max_seq_length, dropout_rate, vocab_size, embedding_size):
+    def __init__(self, max_seq_length, dropout_rate, vocab_size, embedding_size, merge_mode=None):
         super(Model, self).__init__()
 
         self.max_seq_length = max_seq_length
         self.dropout_rate = dropout_rate
         self.vocab_size = vocab_size
         self.embedding_size = embedding_size
+        self.merge_mode = merge_mode
 
-        self.cnn_attn = nn.Sequential(CNN(max_seq_len=max_seq_length, dropout_rate=dropout_rate,
-                                     vocab_size=vocab_size, embedding_dim=embedding_size),
-                                 Attention())
+        self.cnn_attn = nn.Sequential(CNN(max_seq_len=max_seq_length,
+                                          vocab_size=vocab_size, embedding_dim=embedding_size),
+                                      Attention())
 
-        self.bigru_attn = nn.Sequential(BiRNN(model="gru", max_seq_len=max_seq_length, embedding_dim=embedding_size),
-                                   Attention())
+        self.bigru_attn = nn.Sequential(BiRNN(model="gru", max_seq_len=max_seq_length, dropout_rate=dropout_rate,
+                                              merge_mode=merge_mode, embedding_dim=embedding_size),
+                                        Attention())
 
         self.softmax = nn.Softmax()
 
@@ -136,14 +149,27 @@ class Model(nn.Module):
         cnn_res = self.cnn_attn(x)
         bigru_res = self.bigru_attn(x)
 
-        concated_res = torch.concat([cnn_res, bigru_res])
+        concated_res = torch.concat([cnn_res, bigru_res], dim=0)
 
-        res = self.softmax(concated_res)
-
-        return res
-
-    def call(self):
-        res = self.forward()
+        res = self.softmax(concated_res)        # dim=1 추가하기
 
         return res
+
+
+if __name__ == "__main__":
+    merge_mode = "concat"                       # concat, mean, add  --> 더 찾아보기
+    model = Model(max_seq_length=30, dropout_rate=1e-3, vocab_size=300, merge_mode=merge_mode, embedding_size=100)
+    # tmp = torch.rand(3, 30, 100)
+    # print(tmp.size())
+    # x = model(tmp)
+
+    from torchsummary import summary
+
+    # print("print(model): ", model)
+    print("summary(model, (64, 30, 100)): ", summary(model, (30, 100), device="cpu"))
+
+    from torchviz import make_dot
+    x = torch.zeros(1, 30, 100)
+    make_dot(model(x), params=dict(list(model.named_parameters())))
+
 
--- train.py
+++ train.py
@@ -22,13 +22,15 @@ def train_loop(dataloader, model, loss_fn, optimizer):
             loss, current = loss.item(), batch * len(X)
             print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}")
 
+
 def cross_validation(dataloader, model, loss_fn):
+
+    return
 
 
 if __name__ == "__main__":
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
 
     learning_rate = 1e-3
     batch_size = 64        # 임의 지정. 바꾸기.
@@ -38,35 +40,17 @@ if __name__ == "__main__":
     embedding_size = 100    # 여러 차원으로 실험해보기
     max_seq_length = 100    # 논문 언급 없음.
     seed = 1024
+    merge_mode = "concat"
 
-    vocab_size=1751
+    vocab_size = 1751
     num_classes = 2
 
     # Dataloader (Test, validation도 만들기)
     dataset = preprocess.DementiaDataset()
     train_dataloader = DataLoader(dataset, batch_size=batch_size)
 
-    # # Model
-    # cnn = CNN(max_seq_len=max_seq_length, dropout_rate=dropout_rate, vocab_size=vocab_size, embedding_dim=embedding_size)
-    # bigru = BiRNN(model="gru", max_seq_len=max_seq_length, embedding_dim=embedding_size)
-    # attention_cnn = Attention()
-    # attention_bigru = Attention()
+    model = Model(max_seq_length, dropout_rate, vocab_size, merge_mode, embedding_size)
 
-
-
-    # # CNN and Attention
-    # cnn_attn = nn.Sequential(CNN(max_seq_len=max_seq_length, dropout_rate=dropout_rate,
-    #                              vocab_size=vocab_size, embedding_dim=embedding_size),
-    #                          Attention())
-    #
-    # # BiGRU + Attention
-    # bigru_attn = nn.Sequential(BiRNN(model="gru", max_seq_len=max_seq_length, embedding_dim=embedding_size),
-    #                            Attention())
-    #
-    # # concat + Softmax
-    # concated_x = torch.cat([cnn_attn(x), bigru_attn(x)])
-
-    model = Model(max_seq_length, dropout_rate, vocab_size, embedding_size)
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
