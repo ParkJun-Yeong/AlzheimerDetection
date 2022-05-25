02543512cdf0cf5e4bf452e39a0fe1e834de694c
jun-yeong <32181854@dankook.ac.kr>
2022-05-25 19:29:13 +0900

    embedding 참고 자료 주석 추가

--- embedding.py
+++ embedding.py
@@ -3,9 +3,9 @@ import torch
 from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
 
 """
-- Word2Vec
-- Pre-trained Word2Vec
-- Bert Embedding: 문맥에 따라 같은 의미의 단어도 다른 임베딩을 부여. (다른 단어 임베딩은 같은 단어 = 같은 임베딩)
+- Word2Vec (https://wikidocs.net/50739)
+- Bert Embedding: 문맥에 따라 같은 의미의 단어도 다른 임베딩을 부여. (다른 단어 임베딩은 같은 단어 = 같은 임베딩) (https://codlingual.tistory.com/98)
+- Glove (https://wikidocs.net/22885)
 """
 
 corpus_path = "./dataset/corpus.csv"
